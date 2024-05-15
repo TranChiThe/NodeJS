6c4d2d74cdfb227a21e7e0915111546d381d70f3
Thể Trần Chí <tcthe13062002@gmail.com>
2024-05-15 17:10:47 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,25 +1,16 @@
+require('dotenv').config();
 const { render } = require('ejs')
 const express = require('express')
-// const { hostname } = require('os')
-const path = require('path')
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME
-require('dotenv').config();
+const configViewEngine = require('./config/viewEngine')
+const webRoutes = require('./routes/web')
 
-//config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+configViewEngine(app);
 
 // Khai bao cac route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/bookingcare', (req, res) => {
-    // res.send('Hello World!')
-    res.render('sample.ejs')
-})
+app.use('/', webRoutes)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
